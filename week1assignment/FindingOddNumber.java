0bafb33faa77241c2fb3b62434233ab7d37bbd95
suprajasagili111 <168285696+suprajasagili111@users.noreply.github.com>
2024-04-27 21:34:11 +0530

    Add files via upload

---
+++ week1assignment/FindingOddNumber.java
@@ -0,0 +1,17 @@
+package week1assignment;
+
+public class FindingOddNumber {
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for(i=0;i<=20;i++) {
+			
+			if(i%2!=0) {
+			
+				System.out.println("odd numbers from 1 t0 20 are" +"\n"+i);
+			}
+			}
+	}
+
+}
